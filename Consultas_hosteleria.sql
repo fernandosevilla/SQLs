4382cb56f991ef5e1cf8afb3e9d9809e0dca67c8
fernandosevilla <fernandosevilla028@gmail.com>
2024-02-22 13:40:07 +0100

    Terminada consulta 4 con vistas

--- Consultas_hosteleria.sql
+++ Consultas_hosteleria.sql
@@ -1,9 +1,9 @@
 -- 1 Los datos de los camareros (id, apellido y nombre) que han atendido más de 5 mesas
-SELECT DISTINCT c.Id_camarero, c.ape, c.nom, COUNT(caMesas.Id_mesa) AS Mesas_Atendidas
+SELECT DISTINCT c.Id_camarero, c.ape, c.nom, COUNT(*) AS Mesas_Atendidas
 FROM camareros c, Camarero_Mesas caMesas
 WHERE c.Id_camarero = caMesas.Id_camarero
 GROUP BY c.Id_camarero
-HAVING COUNT(caMesas.Id_mesa) > 5;
+HAVING COUNT(*) > 5;
 
 -- 2 Listar parejas de restaurantes que estén en la misma ciudad y que no se repitan
 SELECT DISTINCT r1.Id_rte AS Restaurante1, r2.Id_rte AS Restaurante1, r1.localidad
@@ -28,3 +28,18 @@ NOT IN (SELECT Id_camarero FROM camareros_cena);
 
 -- 4 Listar los camareros con su id, nombre y apellidos que han atendido mas mesas que todos los camareros
 -- que trabajan en el "Restaurante B"
+CREATE OR REPLACE VIEW camareros_restaurante AS 
+SELECT COUNT(*) AS contador
+FROM camarero_mesas cm 
+WHERE cm.Id_rte = 2;
+
+CREATE OR REPLACE VIEW mesas_atendidas AS 
+SELECT Id_camarero, COUNT(*) AS NMesas 
+FROM camarero_mesas cm2
+GROUP BY Id_camarero;
+
+SELECT Id_camarero, ape, nom
+FROM camareros
+WHERE Id_camarero 
+IN (SELECT Id_camarero FROM mesas_atendidas ma, camareros_restaurante cr
+	WHERE ma.NMesas > cr.contador);
